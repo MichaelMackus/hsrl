48e0ed771450aa53bb4bc0568aa37b7238c07606
Michael Mackus <michaelmackus@gmail.com>
2016-06-30 10:26:34 -0700

    Remove need for state in Roller

--- RL/Random.hs
+++ RL/Random.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE DefaultSignatures, DeriveFunctor #-}
 
-module RL.Random (roll, randomPoint, Roller, mkRoller, runRoller) where
+module RL.Random (roll, randomPoint, Roller(..)) where
 
 import RL.Types
 
 import Control.Applicative
 import Control.Monad          (liftM2, ap)
 import Control.Monad.Random   (MonadRandom(..), MonadSplit(..))
-import Control.Monad.State    (StateT(..))
 import System.Random
+
+newtype Roller m a = Roller { runRoller :: StdGen -> m (a, StdGen) } deriving Functor
 
 roll :: MonadRandom m => Dice -> m Int
 roll (D n ns) = getRandomR (minInt, maxInt)
@@ -20,29 +21,21 @@ roll (D n ns) = getRandomR (minInt, maxInt)
 randomPoint :: MonadRandom m => Int -> Int -> m Point
 randomPoint x y = liftM2 (,) (roll $ 1 `d` x) (roll $ 1 `d` y)
 
-newtype Roller m a = Roller { getRand :: StateT StdGen m a } deriving Functor
-
-runRoller :: Monad m => Roller m a -> StdGen -> m (a, StdGen)
-runRoller = runStateT . getRand
-
-mkRoller :: Monad m => (StdGen -> m (a, StdGen)) -> Roller m a
-mkRoller = Roller . StateT
-
 instance Monad m => Monad (Roller m) where
-    return x = mkRoller $ \s -> return (x, s)
-    r >>= f  = mkRoller $ \s -> do
+    return x = Roller $ \s -> return (x, s)
+    r >>= f  = Roller $ \s -> do
         (x, s') <- runRoller r s
         runRoller (f x) s'
 
 instance (Applicative m, Monad m) => Applicative (Roller m) where
-    pure x = mkRoller $ \g -> return (x, g)
+    pure x = Roller $ \g -> return (x, g)
     (<*>)  = ap
 
 instance Monad m => MonadRandom (Roller m) where
-    getRandom     = mkRoller $ \g -> return (random g)
-    getRandoms    = mkRoller $ \g -> return (randoms g, g)
-    getRandomR  r = mkRoller $ \g -> return (randomR r g)
-    getRandomRs r = mkRoller $ \g -> return (randomRs r g, g)
+    getRandom     = Roller $ \g -> return (random g)
+    getRandoms    = Roller $ \g -> return (randoms g, g)
+    getRandomR  r = Roller $ \g -> return (randomR r g)
+    getRandomRs r = Roller $ \g -> return (randomRs r g, g)
 
 instance Monad m => MonadSplit StdGen (Roller m) where
-    getSplit = mkRoller $ \g -> return (g, snd $ next g)
+    getSplit = Roller $ \g -> return (g, snd $ next g)
