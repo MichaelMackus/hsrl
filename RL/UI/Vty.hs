5dae935da7acfb23746fa1740f032c4b268b26e4
Michael Mackus <michaelmackus@gmail.com>
2020-11-22 16:53:39 -0800

    Fix Vty (now with RGB colors)

--- RL/UI/Vty.hs
+++ RL/UI/Vty.hs
@@ -1,9 +1,11 @@
 module RL.UI.Vty (vtyUI) where
 
 import RL.UI.Common
+import RL.Util
 
 import Control.Monad (when)
 import Graphics.Vty
+import qualified Data.List as L
 
 vtyUI :: UIConfig -> IO UI
 vtyUI cfg = do
@@ -15,8 +17,10 @@ vtyUI cfg = do
     disp   <- mkVty vtyCfg
     return UI
         { uiRender = \sprites ->
-            let layers = reverse $ map getImage sprites
-            in  update disp $ picForLayers layers
+            let layers  = reverse (map getImage (condenseSprites sprites))
+                picture = (picForLayers layers) { picBackground = bg }
+                bg      = Background ' ' (withBackColor defAttr (rgbColor 0 0 0))
+            in  update disp picture
         , uiEnd = shutdown disp
         , uiInput = do
             e <- nextEvent disp
@@ -40,6 +44,41 @@ toKeyMods = map toKeyMod
           toKeyMod MMeta  = KeyModAlt
           toKeyMod MAlt   = KeyModSuper
 
+-- faster version but no colors
+-- condenseSprites :: [Sprite] -> [Sprite]
+-- condenseSprites = map getRowSprite . enumerate . unenumerate2d . map sym
+--     where
+--         sym spr = (spritePos spr, head (spriteStr spr))
+--         getRowSprite ((y), ts) = Sprite (0, y) ts (255,255,255) (0,0,0)
+
+-- helper function to condense sprites on the same row & attributes together
+-- TODO improve performance
+condenseSprites = concat . map (map spriteConcat . L.groupBy g) . groupBy2 f
+    where f spr spr'  = let (x,  y ) = spritePos spr
+                            (x', y') = spritePos spr'
+                        in  y == y' && spriteFgColor spr == spriteFgColor spr' && spriteBgColor spr == spriteBgColor spr'
+          g spr spr'  = let (x,  y ) = spritePos spr
+                            (x', y') = spritePos spr'
+                        in  x + 1 == x' && spriteFgColor spr == spriteFgColor spr' && spriteBgColor spr == spriteBgColor spr'
+
+-- concat sprite strings discarding positions of tails
+spriteConcat :: [Sprite] -> Sprite
+spriteConcat [] = error "Empty sprite list for sprite concat"
+spriteConcat (spr:xs) = foldr f spr xs
+    where f spr' spr = spr { spriteStr = spriteStr spr ++ spriteStr spr' }
+
 getImage :: Sprite -> Image
-getImage ((0, 0), str) = string defAttr str
-getImage ((x, y), str) = translate x y $ getImage ((0, 0), str)
+getImage spr = let (x, y) = spritePos spr
+                   (fr,fg,fb) = spriteFgColor spr
+                   (br,bg,bb) = spriteBgColor spr
+                   attr = withBackColor (withForeColor defAttr (rgbColor fr fg fb)) (rgbColor br bg bb)
+               in  translate x y (string attr (spriteStr spr))
+
+-- group by non-adjacent
+-- https://stackoverflow.com/questions/53377577/groupby-function-which-groups-non-adjacent-elements
+groupBy2 :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy2 = go [] where
+  go acc comp [] = acc
+  go acc comp (h:t) =
+    let (hs, nohs) = L.partition (comp h) t
+    in go ((h:hs):acc) comp nohs
